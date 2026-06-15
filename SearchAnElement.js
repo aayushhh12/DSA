466b06ae0ad56f9110b833b5ca64db6d5c61c7fd
aayushhh12 <aayukothari888@gmail.com>
2026-06-15 11:41:16 +0530

    new file created to search an element

---
+++ SearchAnElement.js
@@ -0,0 +1,12 @@
+function searchElement(arr, target) {
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]===target){
+            return i;
+        }
+    }
+    return -1;
+}
+let arr = [1, 2, 3, 4, 5];
+let target = 3;
+let result = searchElement(arr, target);
+console.log(result);

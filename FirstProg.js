a06765a5b5494dc2679770bdfd7aad7112d846ca
aayushhh12 <aayukothari888@gmail.com>
2026-06-15 10:55:19 +0530

    Created Even Number Program

---
+++ FirstProg.js
@@ -0,0 +1,11 @@
+// Even number function
+function evenNum(num) {
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(evenNum(4));
+console.log(evenNum(7));   

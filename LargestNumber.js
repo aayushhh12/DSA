e420100aeeb01e6288c193fcc9da768e070cb57a
aayushhh12 <aayukothari888@gmail.com>
2026-06-15 14:06:09 +0530

    Largest Number printed

---
+++ LargestNumber.js
@@ -0,0 +1,13 @@
+function LargestNumber(arr){
+    let a = arr[0];
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] > a){
+            a = arr[i];
+        }
+    }
+    return a;
+}
+
+let arr = [10, 5, 12, 3, 1, 25];
+let result = LargestNumber(arr);
+console.log(result);

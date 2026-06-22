cb9d11a4233f0aade9d0dfe20200c0e9e45cf7e7
aayushhh12 <aayukothari888@gmail.com>
2026-06-22 12:45:49 +0530

    Binary Search

---
+++ BinarySearch.js
@@ -0,0 +1,22 @@
+function search(nums, target){
+    let left = 0;
+    let right=nums.length - 1;
+
+    while(right >= left){
+        let mid = Math.floor((left + right)/2);
+        if(nums[mid] === target) {
+            return mid;
+        }
+        else if(target < nums[mid]) {
+            right = mid -1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
+let nums = [-2, 0, 3, 6, 8, 10];
+let target = 8;
+let result = search(nums, target)
+console.log(result);
